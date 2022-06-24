bffd9338f10d190f8742523cb6b97c6ca49d85b2
Francisco <gforce3002@gmail.com>
2022-06-24 00:35:02 -0500

    Primer commit

---
+++ ts-intro/src/ejercicios/01-tipos-basicos.ts
@@ -0,0 +1,11 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+let nombre: string = "Francisco";
+let hp: number | string =95;
+let estaVivo: boolean = true
+
+hp = "FULL"
+
+console.log(nombre, hp)
